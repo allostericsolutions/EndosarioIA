d72780bb002d40fd0f73660dc6f19a6d20021ce5
Francisco Curiel de Icaza <ciclosporina2@hotmail.com>
2024-07-30 09:34:33 -0400

    Update image_utils.py

--- file_utils/file_utils/image_utils.py
+++ file_utils/file_utils/image_utils.py
@@ -1,1 +1,17 @@
+from PIL import Image
+import streamlit as st
 
+def mostrar_imagen(image_path, caption, width=None, height=None):
+    """
+    Función para mostrar una imagen con tamaño ajustable en Streamlit.
+    
+    Parámetros:
+    - image_path: Ruta de la imagen.
+    - caption: Título o descripción de la imagen.
+    - width: Ancho deseado de la imagen (en píxeles).
+    - height: Altura deseada de la imagen (en píxeles).
+    """
+    image = Image.open(image_path)
+    
+    # Mostrar la imagen con el ancho y altura especificados (si se proporcionan)
+    st.image(image, caption=caption, use_column_width=False, width=width, height=height)
