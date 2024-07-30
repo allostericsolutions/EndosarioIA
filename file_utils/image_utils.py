f3bd9ef86911ec4ec7c2f58ab63d2354c424a86f
Francisco Curiel de Icaza <ciclosporina2@hotmail.com>
2024-07-30 10:59:11 -0400

    Update image_utils.py

--- file_utils/image_utils.py
+++ file_utils/image_utils.py
@@ -6,7 +6,7 @@ def mostrar_imagen(image_path, caption, width=None, height=None):
     Función para mostrar una imagen con tamaño ajustable en Streamlit.
     
     Parámetros:
-    - image_path: Ruta de la imagen.
+    - image_path: Allosteric_Solutions.png.
     - caption: Título o descripción de la imagen.
     - width: Ancho deseado de la imagen (en píxeles).
     - height: Altura deseada de la imagen (en píxeles).
