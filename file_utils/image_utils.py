2ea241ac9112f7e206e398939c04cc927b2994fc
Francisco Curiel de Icaza <ciclosporina2@hotmail.com>
2024-07-30 11:14:55 -0400

    Update image_utils.py

--- file_utils/image_utils.py
+++ file_utils/image_utils.py
@@ -1,7 +1,7 @@
 from PIL import Image
 import streamlit as st
 
-def mostrar_imagen(image_path, caption, width=None, height=None):
+def mostrar_imagen(image_path, caption, width=None):
     """
     Función para mostrar una imagen con tamaño ajustable en Streamlit.
 
@@ -9,26 +9,3 @@ def mostrar_imagen(image_path, caption, width=None, height=None):
     - image_path: Ruta al archivo de imagen.
     - caption: Título o descripción de la imagen.
     - width: Ancho deseado de la imagen (en píxeles).
-    - height: Altura deseada de la imagen (en píxeles).
-    """
-    try:
-        # Verificar y abrir la imagen
-        if not isinstance(image_path, str):
-            raise TypeError("`image_path` debe ser una cadena de texto que represente la ruta de la imagen")
-        image = Image.open(image_path)
-        
-        # Verificar el tipo de caption
-        if not isinstance(caption, str):
-            raise TypeError("`caption` debe ser una cadena de texto")
-        
-        # Verificar el tipo de width
-        if width is not None and not isinstance(width, int):
-            raise TypeError("`width` debe ser un número entero o None")
-        
-        # Verificar el tipo de height
-        if height is not None and not isinstance(height, int):
-            raise TypeError("`height` debe ser un número entero o None")
-        
-        st.image(image, caption=caption, use_column_width=False, width=width, height=height)
-    except Exception as e:
-        st.error(f"Error al mostrar la imagen: {e}")
