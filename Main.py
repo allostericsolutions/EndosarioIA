0470ce1bacd00f7d798fab30b48f7b63b155de85
Francisco Curiel de Icaza <ciclosporina2@hotmail.com>
2024-08-03 12:25:18 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,198 +1,198 @@
- import streamlit as st
-   from pdfminer.high_level import extract_text
-   from fpdf import FPDF
-   import pandas as pd
-   import io
-   import re
-   import difflib
-   from PIL import Image
-   from sklearn.feature_extraction.text import TfidfVectorizer
-   from sklearn.metrics.pairwise import cosine_similarity
-   from openpyxl.utils.exceptions import IllegalCharacterError
-   from text_processing import extract_and_clean_text
-   from file_utils.file_creators import create_excel, create_csv, create_txt
-   from file_utils.image_utils import mostrar_imagen
-   from bot.chat_handler import initialize_chat_state, simulate_greeting, handle_chat
+import streamlit as st
+from pdfminer.high_level import extract_text
+from fpdf import FPDF
+import pandas as pd
+import io
+import re
+import difflib
+from PIL import Image
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from openpyxl.utils.exceptions import IllegalCharacterError
+from text_processing import extract_and_clean_text
+from file_utils.file_creators import create_excel, create_csv, create_txt
+from file_utils.image_utils import mostrar_imagen
+from bot.chat_handler import initialize_chat_state, simulate_greeting, handle_chat
 
-   # Inicializar las configuraciones de OpenAI
-   client = initialize_openai()
+# Inicializar las configuraciones de OpenAI
+client = initialize_openai()
 
-   # Configurar los parámetros de la página, incluyendo el nuevo icono
-   st.set_page_config(page_title="Endosario Móvil AI 2.0", page_icon="ícono robot.png")
+# Configurar los parámetros de la página, incluyendo el nuevo icono
+st.set_page_config(page_title="Endosario Móvil AI 2.0", page_icon="ícono robot.png")
 
-   # Título de la aplicación en la página principal
-   st.title("Endosario Móvil AI 2.0")
+# Título de la aplicación en la página principal
+st.title("Endosario Móvil AI 2.0")
 
-   # Mostrar la imagen y el título en la barra lateral
-   with st.sidebar.expander("Información", expanded=True):
-       st.markdown("### Endosario Móvil AI 2.0")
-       image_path = 'Allosteric_Solutions.png'
-       caption = 'Interesse'
-       width = 300
-       mostrar_imagen(image_path, caption, width)
+# Mostrar la imagen y el título en la barra lateral
+with st.sidebar.expander("Información", expanded=True):
+    st.markdown("### Endosario Móvil AI 2.0")
+    image_path = 'Allosteric_Solutions.png'
+    caption = 'Interesse'
+    width = 300
+    mostrar_imagen(image_path, caption, width)
 
-   # Subir los dos archivos PDF
-   uploaded_file_1 = st.file_uploader("Modelo", type=["pdf"], key="uploader1")
-   uploaded_file_2 = st.file_uploader("Verificación", type=["pdf"], key="uploader2")
+# Subir los dos archivos PDF
+uploaded_file_1 = st.file_uploader("Modelo", type=["pdf"], key="uploader1")
+uploaded_file_2 = st.file_uploader("Verificación", type=["pdf"], key="uploader2")
 
-   # Variables para manejar el estado de los archivos subidos
-   archivo_subido_1 = False
-   archivo_subido_2 = False
+# Variables para manejar el estado de los archivos subidos
+archivo_subido_1 = False
+archivo_subido_2 = False
 
-   # Verificar si los archivos han sido subidos y extraer el texto
-   if uploaded_file_1:
-       archivo_subido_1 = True
-       text_by_code_1, unique_code_count_1, codes_model = extract_and_clean_text(uploaded_file_1)
-   if uploaded_file_2:
-       archivo_subido_2 = True
-       text_by_code_2, unique_code_count_2, _ = extract_and_clean_text(uploaded_file_2)
+# Verificar si los archivos han sido subidos y extraer el texto
+if uploaded_file_1:
+    archivo_subido_1 = True
+    text_by_code_1, unique_code_count_1, codes_model = extract_and_clean_text(uploaded_file_1)
+if uploaded_file_2:
+    archivo_subido_2 = True
+    text_by_code_2, unique_code_count_2, _ = extract_and_clean_text(uploaded_file_2)
 
-   # Botón para reiniciar la aplicación
-   if st.sidebar.button("Reiniciar"):
-       archivo_subido_1 = False
-       archivo_subido_2 = False
-       st.session_state.chat_history = []
-       st.session_state.analysis_loaded = False
-       st.session_state.saludo_enviado = False  # Reiniciar el estado del saludo
+# Botón para reiniciar la aplicación
+if st.sidebar.button("Reiniciar"):
+    archivo_subido_1 = False
+    archivo_subido_2 = False
+    st.session_state.chat_history = []
+    st.session_state.analysis_loaded = False
+    st.session_state.saludo_enviado = False  # Reiniciar el estado del saludo
 
-   # Mostrar la sección de comparación de archivos solo si se han subido ambos archivos
-   if archivo_subido_1 and archivo_subido_2:
-       # Obtener todos los códigos únicos presentes en ambos documentos
-       all_codes = set(text_by_code_1.keys()).union(set(text_by_code_2.keys()))
+# Mostrar la sección de comparación de archivos solo si se han subido ambos archivos
+if archivo_subido_1 and archivo_subido_2:
+    # Obtener todos los códigos únicos presentes en ambos documentos
+    all_codes = set(text_by_code_1.keys()).union(set(text_by_code_2.keys()))
 
-       # Función para manejar texto largo en el campo del endoso
-       def handle_long_text(text, length=70):
-           if len(text) > length:
-               return f'<details><summary>Endoso</summary>{text}</details>'
-           else:
-               return text
+    # Función para manejar texto largo en el campo del endoso
+    def handle_long_text(text, length=70):
+        if len(text) > length:
+            return f'<details><summary>Endoso</summary>{text}</details>'
+        else:
+            return text
 
-       # Crear la tabla comparativa
-       comparison_data = []
-       for code in all_codes:
-           doc1_text = text_by_code_1.get(code, "Ausente")
-           doc1_text_display = handle_long_text(doc1_text)
-           doc2_text = text_by_code_2.get(code, "Ausente")
-           doc2_text_display = handle_long_text(doc2_text)
+    # Crear la tabla comparativa
+    comparison_data = []
+    for code in all_codes:
+        doc1_text = text_by_code_1.get(code, "Ausente")
+        doc1_text_display = handle_long_text(doc1_text)
+        doc2_text = text_by_code_2.get(code, "Ausente")
+        doc2_text_display = handle_long_text(doc2_text)
 
-           # Si un texto no está presente, el porcentaje de similitud textual es 0
-           if doc1_text == "Ausente" or doc2_text == "Ausente":
-               sim_percentage = 0
-               similarity_str = "0.00%"
-           else:
-               sim_percentage = calculate_semantic_similarity(doc1_text, doc2_text)
-               similarity_str = f'{sim_percentage:.2f}%'
+        # Si un texto no está presente, el porcentaje de similitud textual es 0
+        if doc1_text == "Ausente" or doc2_text == "Ausente":
+            sim_percentage = 0
+            similarity_str = "0.00%"
+        else:
+            sim_percentage = calculate_semantic_similarity(doc1_text, doc2_text)
+            similarity_str = f'{sim_percentage:.2f}%'
 
-           # Si un número no está presente, el porcentaje de similitud numérica es 0
-           if doc1_text == "Ausente" or doc2_text == "Ausente":
-               num_similarity_percentage = 0
-               doc1_num_display = "Ausente"
-               doc2_num_display = "Ausente"
-           else:
-               doc1_num, doc1_context, doc2_num, doc2_context = extract_and_align_numbers_with_context(doc1_text, doc2_text)
-               doc1_num_display = f'<details><summary>{doc1_num}</summary><p>{doc1_context}</p></details>'
-               doc2_num_display = f'<details><summary>{doc2_num}</summary><p>{doc2_context}</p></details>'
-               num_similarity_percentage = calculate_numbers_similarity(doc1_num, doc2_num)
+        # Si un número no está presente, el porcentaje de similitud numérica es 0
+        if doc1_text == "Ausente" or doc2_text == "Ausente":
+            num_similarity_percentage = 0
+            doc1_num_display = "Ausente"
+            doc2_num_display = "Ausente"
+        else:
+            doc1_num, doc1_context, doc2_num, doc2_context = extract_and_align_numbers_with_context(doc1_text, doc2_text)
+            doc1_num_display = f'<details><summary>{doc1_num}</summary><p>{doc1_context}</p></details>'
+            doc2_num_display = f'<details><summary>{doc2_num}</summary><p>{doc2_context}</p></details>'
+            num_similarity_percentage = calculate_numbers_similarity(doc1_num, doc2_num)
 
-           # Agregar los datos a la tabla comparativa
-           row = {
-               "Código": f'<b><span style="color:red;">{code}</span></b>',
-               "Documento Modelo": doc1_text_display if doc1_text != "Ausente" else f'<b style="color:red;">Ausente</b>',
-               "Valores numéricos Modelo": f'<details><summary>Contexto</summary>{doc1_num_display}</details>',
-               "Documento Verificación": doc2_text_display if doc2_text != "Ausente" else f'<b style="color:red;">Ausente</b>',
-               "Valores numéricos Verificación": f'<details><summary>Contexto</summary>{doc2_num_display}</details>',
-               "Similitud Texto": similarity_str,
-               "Similitud Numérica": f'{num_similarity_percentage:.2f}%'
-           }
-           comparison_data.append(row)
+        # Agregar los datos a la tabla comparativa
+        row = {
+            "Código": f'<b><span style="color:red;">{code}</span></b>',
+            "Documento Modelo": doc1_text_display if doc1_text != "Ausente" else f'<b style="color:red;">Ausente</b>',
+            "Valores numéricos Modelo": f'<details><summary>Contexto</summary>{doc1_num_display}</details>',
+            "Documento Verificación": doc2_text_display if doc2_text != "Ausente" else f'<b style="color:red;">Ausente</b>',
+            "Valores numéricos Verificación": f'<details><summary>Contexto</summary>{doc2_num_display}</details>',
+            "Similitud Texto": similarity_str,
+            "Similitud Numérica": f'{num_similarity_percentage:.2f}%'
+        }
+        comparison_data.append(row)
 
-       # Convertir la lista a DataFrame
-       comparison_df = pd.DataFrame(comparison_data)
+    # Convertir la lista a DataFrame
+    comparison_df = pd.DataFrame(comparison_data)
 
-       # Generar HTML para la tabla con estilización CSS
-       def generate_html_table(df):
-           html = df.to_html(index=False, escape=False, render_links=True)
-           html = html.replace(
-               '<table border="1" class="dataframe">',
-               '<table border="1" class="dataframe" style="width:100%; border-collapse:collapse;">'
-           ).replace(
-               '<thead>',
-               '<thead style="position: sticky; top: 0; z-index: 1; background: #fff;">'
-           ).replace(
-               '<th>',
-               '<th class="fixed-width" style="background-color:#f2f2f2; padding:10px; text-align:left; z-index: 1;">'
-           ).replace(
-               '<td>',
-               '<td class="fixed-width" style="border:1px solid black; padding:10px; text-align:left; vertical-align:top;">'
-           )
-           # Aplica estilos a "Documento Modelo" y "Documento Verificación"
-           html = html.replace(
-               '<th>Documento Modelo</th>',
-               '<th style="font-size: 20px; font-weight: bold;">Documento Modelo</th>'
-           )
-           html = html.replace(
-               '<th>Documento Verificación</th>',
-               '<th style="font-size: 20px; font-weight: bold;">Documento Verificación</th>'
-           )
-           # Agrega estilos CSS para las celdas de similitud numérica
-           df["Similitud Numérica"] = df["Similitud Numérica"].str.rstrip('%').astype(float)
-           df["Similitud Numérica"] = df["Similitud Numérica"].apply(lambda x: f"{x:.2f}%")
-           for i, row in df.iterrows():
-               html = html.replace(
-                   f'<td class="fixed-width" style="border:1px solid black; padding:10px; text-align:left; vertical-align:top;">{row["Similitud Numérica"]}%</td>',
-                   f'<td class="fixed-width" style="border:1px solid black; padding:10px; text-align:left; vertical-align:top;">{row["Similitud Numérica"]}</td>'
-               )
-           return html
+    # Generar HTML para la tabla con estilización CSS
+    def generate_html_table(df):
+        html = df.to_html(index=False, escape=False, render_links=True)
+        html = html.replace(
+            '<table border="1" class="dataframe">',
+            '<table border="1" class="dataframe" style="width:100%; border-collapse:collapse;">'
+        ).replace(
+            '<thead>',
+            '<thead style="position: sticky; top: 0; z-index: 1; background: #fff;">'
+        ).replace(
+            '<th>',
+            '<th class="fixed-width" style="background-color:#f2f2f2; padding:10px; text-align:left; z-index: 1;">'
+        ).replace(
+            '<td>',
+            '<td class="fixed-width" style="border:1px solid black; padding:10px; text-align:left; vertical-align:top;">'
+        )
+        # Aplica estilos a "Documento Modelo" y "Documento Verificación"
+        html = html.replace(
+            '<th>Documento Modelo</th>',
+            '<th style="font-size: 20px; font-weight: bold;">Documento Modelo</th>'
+        )
+        html = html.replace(
+            '<th>Documento Verificación</th>',
+            '<th style="font-size: 20px; font-weight: bold;">Documento Verificación</th>'
+        )
+        # Agrega estilos CSS para las celdas de similitud numérica
+        df["Similitud Numérica"] = df["Similitud Numérica"].str.rstrip('%').astype(float)
+        df["Similitud Numérica"] = df["Similitud Numérica"].apply(lambda x: f"{x:.2f}%")
+        for i, row in df.iterrows():
+            html = html.replace(
+                f'<td class="fixed-width" style="border:1px solid black; padding:10px; text-align:left; vertical-align:top;">{row["Similitud Numérica"]}%</td>',
+                f'<td class="fixed-width" style="border:1px solid black; padding:10px; text-align:left; vertical-align:top;">{row["Similitud Numérica"]}</td>'
+            )
+        return html
 
-       # Convertir DataFrame a HTML con estilización CSS y HTML modificado
-       table_html = generate_html_table(comparison_df)
-       st.markdown("### Comparación de Documentos")
-       st.markdown(table_html, unsafe_allow_html=True)
+    # Convertir DataFrame a HTML con estilización CSS y HTML modificado
+    table_html = generate_html_table(comparison_df)
+    st.markdown("### Comparación de Documentos")
+    st.markdown(table_html, unsafe_allow_html=True)
 
-       # Mostrar el conteo de códigos únicos en cada documento
-       st.markdown("### Conteo de Códigos")
-       st.write(f"**Documento Modelo:** {unique_code_count_1} (Faltan: {', '.join(list(all_codes - set(codes_model)))})")
-       st.write(f"**Documento Verificación:** {unique_code_count_2} (Faltan: {', '.join(list(all_codes - set(text_by_code_2.keys())))})")
+    # Mostrar el conteo de códigos únicos en cada documento
+    st.markdown("### Conteo de Códigos")
+    st.write(f"**Documento Modelo:** {unique_code_count_1} (Faltan: {', '.join(list(all_codes - set(codes_model)))})")
+    st.write(f"**Documento Verificación:** {unique_code_count_2} (Faltan: {', '.join(list(all_codes - set(text_by_code_2.keys())))})")
 
-       # Botones para descargar los archivos de comparación en diferentes formatos
-       col1, col2, col3 = st.columns(3)
-       with col1:
-           download_excel = st.button("Download Comparison Excel")
-           if download_excel:
-               excel_buffer = create_excel(comparison_df)
-               st.download_button(
-                   label="Descarga Excel",
-                   data=excel_buffer,
-                   file_name="comparison.xlsx",
-                   mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-               )
-       with col2:
-           download_csv = st.button("Download Comparison CSV")
-           if download_csv:
-               csv_buffer = create_csv(comparison_df)
-               st.download_button(
-                   label="Descarga CSV",
-                   data=csv_buffer,
-                   file_name="comparison.csv",
-                   mime="text/csv"
-               )
-       with col3:
-           download_txt = st.button("Download Comparison TXT")
-           if download_txt:
-               txt_buffer = create_txt(comparison_df, unique_code_count_1, unique_code_count_2)
-               st.download_button(
-                   label="Descarga TXT",
-                   data=txt_buffer,
-                   file_name="comparison.txt",
-                   mime="text/plain"
-               )
+    # Botones para descargar los archivos de comparación en diferentes formatos
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        download_excel = st.button("Download Comparison Excel")
+        if download_excel:
+            excel_buffer = create_excel(comparison_df)
+            st.download_button(
+                label="Descarga Excel",
+                data=excel_buffer,
+                file_name="comparison.xlsx",
+                mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+            )
+    with col2:
+        download_csv = st.button("Download Comparison CSV")
+        if download_csv:
+            csv_buffer = create_csv(comparison_df)
+            st.download_button(
+                label="Descarga CSV",
+                data=csv_buffer,
+                file_name="comparison.csv",
+                mime="text/csv"
+            )
+    with col3:
+        download_txt = st.button("Download Comparison TXT")
+        if download_txt:
+            txt_buffer = create_txt(comparison_df, unique_code_count_1, unique_code_count_2)
+            st.download_button(
+                label="Descarga TXT",
+                data=txt_buffer,
+                file_name="comparison.txt",
+                mime="text/plain"
+            )
 
-       # --- Sección para la IA ---
-       st.markdown("### InteresseAssist Bot")
+    # --- Sección para la IA ---
+    st.markdown("### InteresseAssist Bot")
 
-       # Inicializar el estado del chat y simular el saludo
-       initialize_chat_state()
-       simulate_greeting()
+    # Inicializar el estado del chat y simular el saludo
+    initialize_chat_state()
+    simulate_greeting()
 
-       # Manejar el chat con el bot
-       handle_chat()
+    # Manejar el chat con el bot
+    handle_chat()
