61eaa5b2e6c0d3b4aa34e680f9618ae65fbe3810
Francisco Curiel de Icaza <ciclosporina2@hotmail.com>
2024-08-20 12:22:22 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -15,7 +15,7 @@ from file_utils.image_utils import mostrar_imagen
 from gpt_config.openai_setup import initialize_openai
 from file_utils.text_processing.text_processing import preprocess_text, calculate_semantic_similarity, extract_and_align_numbers_with_context, calculate_numbers_similarity
 
-# Inicializar las configuraciones de OpenAI AllostericSolutions
+# Inicializar las configuraciones de OpenAI
 client = initialize_openai()
 
 # Configurar los parámetros de la página, incluyendo el nuevo icono
@@ -24,7 +24,7 @@ st.set_page_config(page_title="Endosario Móvil AI 2.1", page_icon="ícono robot
 # Título de la aplicación en la página principal
 st.title("Endosario Móvil AI 2.1")
 
-# Mostrar la imagen y el título en la barra lateral AllostericSolutions
+# Mostrar la imagen y el título en la barra lateral
 with st.sidebar.expander("Información", expanded=True):
     st.markdown("### Endosario Móvil AI 2.1")
     image_path = 'Allosteric_Solutions.png'
@@ -36,11 +36,11 @@ with st.sidebar.expander("Información", expanded=True):
 uploaded_file_1 = st.file_uploader("PEI", type=["pdf"], key="uploader1")
 uploaded_file_2 = st.file_uploader("Metlife", type=["pdf"], key="uploader2")
 
-# Variables para manejar el estado de los archivos subidos AllostericSolutions
+# Variables para manejar el estado de los archivos subidos
 archivo_subido_1 = False
 archivo_subido_2 = False
 
-# Verificar si los archivos han sido subidos y extraer el texto AllostericSolutions
+# Verificar si los archivos han sido subidos y extraer el texto
 if uploaded_file_1:
     archivo_subido_1 = True
     text_by_code_1, unique_code_count_1, codes_model = extract_and_clean_text(uploaded_file_1)
@@ -48,7 +48,7 @@ if uploaded_file_2:
     archivo_subido_2 = True
     text_by_code_2, unique_code_count_2, _ = extract_and_clean_text(uploaded_file_2)
 
-# Botón para reiniciar la aplicación AllostericSolutions
+# Botón para reiniciar la aplicación
 if st.sidebar.button("Reiniciar"):
     archivo_subido_1 = False
     archivo_subido_2 = False
@@ -56,20 +56,24 @@ if st.sidebar.button("Reiniciar"):
     st.session_state.analysis_loaded = False
     st.session_state.saludo_enviado = False  # Reiniciar el estado del saludo
 
-# Mostrar la sección de comparación de archivos solo si se han subido ambos archivos AllostericSolutions
+# Leer el archivo CSV con los nombres de los endosos
+endosos_df = pd.read_csv('endosos.csv')
+endosos_dict = dict(zip(endosos_df['codigo'], endosos_df['nombre']))
+
+# Mostrar la sección de comparación de archivos solo si se han subido ambos archivos
 if archivo_subido_1 and archivo_subido_2:
     
-    # Obtener todos los códigos únicos presentes en ambos documentos AllostericSolutions
+    # Obtener todos los códigos únicos presentes en ambos documentos
     all_codes = set(text_by_code_1.keys()).union(set(text_by_code_2.keys()))
 
-    # Función para manejar texto largo en el campo del endoso AllostericSolutions
+    # Función para manejar texto largo en el campo del endoso
     def handle_long_text(text, length=70):
         if len(text) > length:
             return f'<details><summary>Endoso</summary>{text}</details>'
         else:
             return text
 
-    # Crear la tabla comparativa AllostericSolutions
+    # Crear la tabla comparativa
     comparison_data = []
     for code in all_codes:
         doc1_text = text_by_code_1.get(code, "Ausente")
@@ -77,7 +81,7 @@ if archivo_subido_1 and archivo_subido_2:
         doc2_text = text_by_code_2.get(code, "Ausente")
         doc2_text_display = handle_long_text(doc2_text)
 
-        # Si un texto no está presente, el porcentaje de similitud textual es 0  AllostericSolutions
+        # Si un texto no está presente, el porcentaje de similitud textual es 0
         if doc1_text == "Ausente" or doc2_text == "Ausente":
             sim_percentage = 0
             similarity_str = "0.00%"
@@ -85,7 +89,7 @@ if archivo_subido_1 and archivo_subido_2:
             sim_percentage = calculate_semantic_similarity(doc1_text, doc2_text)
             similarity_str = f'{sim_percentage:.2f}%'
 
-        # Si un número no está presente, el porcentaje de similitud numérica es 0  AllostericSolutions
+        # Si un número no está presente, el porcentaje de similitud numérica es 0
         if doc1_text == "Ausente" or doc2_text == "Ausente":
             num_similarity_percentage = 0
             doc1_num_display = "Ausente"
@@ -96,9 +100,13 @@ if archivo_subido_1 and archivo_subido_2:
             doc2_num_display = f'<details><summary>{doc2_num}</summary><p>{doc2_context}</p></details>'
             num_similarity_percentage = calculate_numbers_similarity(doc1_num, doc2_num)
 
-        # Agregar los datos a la tabla comparativa  AllostericSolutions
+        # Obtener el nombre del endoso
+        nombre_endoso = endosos_dict.get(code, "NA")
+
+        # Agregar los datos a la tabla comparativa
         row = {
             "Código": f'<b><span style="color:red;">{code}</span></b>',
+            "Nombre del Endoso": nombre_endoso,
             "Documento PEI": f'<span style="font-size:16px; font-weight:bold;">{doc1_text_display if doc1_text != "Ausente" else f"<b style=\'color:red;\'>Ausente</b>"}</span>',
             "Valores numéricos PEI": f'<details><summary>Contexto</summary>{doc1_num_display}</details>',
             "Documento Metlife": f'<span style="font-size:16px; font-weight:bold;">{doc2_text_display if doc2_text != "Ausente" else f"<b style=\'color:red;\'>Ausente</b>"}</span>',
@@ -108,10 +116,10 @@ if archivo_subido_1 and archivo_subido_2:
         }
         comparison_data.append(row)
 
-    # Convertir la lista a DataFrame AllostericSolutions
+    # Convertir la lista a DataFrame
     comparison_df = pd.DataFrame(comparison_data)
 
-    # Generar HTML para la tabla con estilización CSS AllostericSolutions
+    # Generar HTML para la tabla con estilización CSS
     def generate_html_table(df):
         html = df.to_html(index=False, escape=False, render_links=True)
         html = html.replace(
