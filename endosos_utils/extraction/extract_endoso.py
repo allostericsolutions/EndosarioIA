35428eaae4072df23db8300664a9fb0fcb72b10b
Francisco Curiel de Icaza <ciclosporina2@hotmail.com>
2024-08-19 23:29:06 -0400

    Update extract_endoso.py

--- endosos_utils/extraction/extract_endoso.py
+++ endosos_utils/extraction/extract_endoso.py
@@ -1,3 +1,5 @@
+# endosos_utils/extraction/extract_endoso.py
+
 import re
 
 def extract_endoso_names(text):
@@ -10,7 +12,6 @@ def extract_endoso_names(text):
     Returns:
         dict: Diccionario con códigos como claves y nombres de endoso como valores.
     """
-    # Patrón para identificar los códigos y capturar la segunda línea en mayúsculas que sigue
     pattern = r'(\b[A-Z]{2}\.\d{3}\.\d{3}\b)\s+([A-Z\s]+)\n([A-Z\s]+)'
     matches = re.findall(pattern, text)
     endoso_names = {match[0]: match[2].strip().lower().capitalize() for match in matches}
