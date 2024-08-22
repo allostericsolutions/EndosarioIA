98dee59b582749446326ca4bc39004a31228f538
Francisco Curiel de Icaza <ciclosporina2@hotmail.com>
2024-08-22 10:37:33 -0400

    Delete file_utils/file_handling.py

--- file_utils/file_handling.py
+++
@@ -1,7 +0,0 @@
-from file_utils.pdf_utils import subir_archivos, verificar_archivos
-
-def initialize_files():
-    return subir_archivos()
-
-def verify_files(file1, file2):
-    return verificar_archivos(file1, file2)
